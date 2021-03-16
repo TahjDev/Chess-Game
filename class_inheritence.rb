8f84e4be6e7a0d96a68615064d634849e7869890
min <minwang.bus@gmail.com>
2021-03-16 14:10:25 -0400

    add class inheritence

--- class_inheritence.rb
+++ class_inheritence.rb
@@ -1,4 +1,5 @@
 class Employee
+    attr_reader :salary
     def initialize(name, title, salary, boss)
         @name = name
         @title = title
@@ -6,13 +7,47 @@ class Employee
         @boss = boss
     end
 
+    def bonus(mutiplier)
+        salary * mutiplier
+    end
 end
 
 
 class Manager < Employee
+
+    attr_accessor :employees
+
     def initialize(name, title, salary, boss=nil)
         super
         @employees = []
     end
     
+    def bonus(mutiplier)
+        que = @employees.dup
+        tot_salary = 0
+        until que.empty?
+            employee = que.shift
+
+            if employee.is_a?(Manager)
+                 tot_salary += employee.salary
+                 que += employee.employees
+            else 
+                tot_salary += employee.salary
+            end 
+        end
+        tot_salary * mutiplier
+    end
 end
+
+
+# ned = Manager.new("ned", "founder", 1000000)
+# darren = Manager.new("darren", "manager", 78000, "ned")
+# shawn = Employee.new("shawn", "TA", 12000, "darren")
+# david = Employee.new("david", "TA", 10000, "darren")
+
+# ned.employees += [darren] 
+# darren.employees += [shawn, david] 
+
+# p ned.bonus(5) # => 500_000
+# p darren.bonus(4) # => 88_000
+# p david.bonus(3) # => 30_000
