d1f76a9c1f14a518ba8b7ec3e5f6622bd3f09966
tahj <tahjharris00@gmail.com>
2021-03-16 16:15:02 -0500

    add functionality to module slideable

--- chess/pieces/slideable.rb
+++ chess/pieces/slideable.rb
@@ -1,12 +1,22 @@
 module Slideable
 
     HORIZONTAL_DIRS = [
-       [1, 0]
+        [1, 0]
         [-1, 0]
         [0, 1]
         [0, -1]
     ]
 
-    
-
+    DIAGONAL_DIRS = [
+        [-1,-1]
+        [1,1]
+        [1,-1]
+        [-1,1]
+    ]
+    def moves
+        move = []
+        move_dirs.each do |dx, dy|
+            
+        end
+    end
 end
